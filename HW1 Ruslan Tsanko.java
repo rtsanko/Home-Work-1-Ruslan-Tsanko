f55ba98b182b59f65fa276a3945db5b371c2f552
rtsa <r.tsanko@rtsa.com>
2018-08-03 11:23:30 +0300

    home work

---
+++ HW1 Ruslan Tsanko.java
@@ -0,0 +1,20 @@
+public class HW1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 10;
+        if (x<=10) {
+            System.out.println(x);
+        } else {
+        System.out.println("x>10");
+        }
+        for (int y = 0; y<=100; y++){
+            System.out.println(y);
+        }
+        int z = 0;
+        while (z<=100){
+            System.out.println(z);
+            z++;
+        }
+    }
+
+}
